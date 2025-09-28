0de5163c7bc1f9948beb7299f62bba6c0dec05e9
c3iv3r <kaelzsec@gmail.com>
2025-09-29 00:08:21 +0700

    Update fishdev.lua

--- dev/fishdev.lua
+++ dev/fishdev.lua
@@ -623,7 +623,7 @@ local loadedCount, totalCount = FeatureManager:InitializeAllFeatures()
 --- === WINDOW === ---
 local Window = Noctis:CreateWindow({
     Title         = "<b>Noctis</b>",
-    Footer        = "Fish It | v1.1.8",
+    Footer        = "Fish It | v1.1.9",
     Icon          = "rbxassetid://123156553209294",
     NotifySide    = "Right",
     IconSize      = UDim2.fromOffset(30, 30),

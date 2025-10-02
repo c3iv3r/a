f557fe17b7a8c0078bac6afbb3faba7b6ae10b4a
c3iv3r <kaelzsec@gmail.com>
2025-10-02 15:57:09 +0700

    Update fishit.lua

--- pub/fishit.lua
+++ pub/fishit.lua
@@ -77,7 +77,7 @@ mainLogger:info(string.format("Features ready: %d/%d", loadedCount, totalCount))
 --- === WINDOW === ---
 local Window = Noctis:CreateWindow({
     Title         = "<b>Noctis</b>",
-    Footer        = "Fish It | v0.1.8",
+    Footer        = "Fish It | v0.1.9",
     Icon          = "rbxassetid://123156553209294",
     NotifySide    = "Right",
     IconSize      = UDim2.fromOffset(30, 30),
@@ -107,7 +107,7 @@ local TabSetting         = Window:AddTab("Setting", "settings")
 
 --- === CHANGELOG & DISCORD LINK === ---
 local CHANGELOG = table.concat({
-    "[+] Added Save GPU (Blank Screen)"
+    "[/] Improved Anti AFK"
 }, "\n")
 local DISCORD = table.concat({
     "https://discord.gg/3AzvRJFT3M",

8a20d54a42cce51fe05c24af34b7f7e9e9bd54b1
c3iv3r <kaelzsec@gmail.com>
2025-10-05 14:46:55 +0700

    ss

--- dev/fishdev.lua
+++ dev/fishdev.lua
@@ -77,7 +77,7 @@ mainLogger:info(string.format("Features ready: %d/%d", loadedCount, totalCount))
 --- === WINDOW === ---
 local Window = Noctis:CreateWindow({
     Title         = "<b>Noctis</b>",
-    Footer        = "Fish It | v1.6.3",
+    Footer        = "Fish It | v1.6.4",
     Icon          = "rbxassetid://123156553209294",
     NotifySide    = "Right",
     IconSize      = UDim2.fromOffset(30, 30),

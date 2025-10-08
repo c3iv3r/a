055edb75afadd7a91043902b487178d5d6ead614
c3iv3r <kaelzsec@gmail.com>
2025-10-08 13:53:59 +0700

    sas

--- dev/fishdevframework.lua
+++ dev/fishdevframework.lua
@@ -1149,7 +1149,7 @@ savepostele_btn = TelePosSec:Button({
     Callback = function()
         if not positionManagerFeature then return end
         
-        local selectedPos = savepos_dd:SetValue()
+        local selectedPos = savepos_dd.Value
         if not selectedPos or selectedPos == "No Positions" then
             Noctis:Notify({
                 Title = "Position Teleport",
@@ -1203,7 +1203,7 @@ saveposdel_btn = TelePosSec:Button({
     Callback = function()
         if not positionManagerFeature then return end
         
-        local selectedPos = savepos_dd:SetValue()
+        local selectedPos = savepos_dd.Value
         if not selectedPos or selectedPos == "No Positions" then
             Noctis:Notify({
                 Title = "Position Teleport",
